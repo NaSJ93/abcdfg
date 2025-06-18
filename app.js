5cdce0abc868cdefeec2279dcd7a77efe4c4360a
NaSJ93 <kyronares@naver.com>
2025-06-18 16:37:31 +0900

    '/nsj' 추가

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-
+app.use('/nsj',(req,res,next)=>{
+  console.log('test')
+})
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
